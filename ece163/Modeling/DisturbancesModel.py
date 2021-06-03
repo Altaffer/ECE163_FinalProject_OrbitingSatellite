082386296616fc301d88a9aac8ce8bbd34d18703
taltaffe <taltaffe@ucsc.edu>
2021-06-03 15:20:28 -0700

    disturbances model tested and finished

--- ece163/Modeling/DisturbancesModel.py
+++ ece163/Modeling/DisturbancesModel.py
@@ -16,41 +16,49 @@ def distanceFromMoon(state):
     throughout orbit using Earth distances as reference points. Returns normalized vector.
     """
     #finding the difference between earthMoon and earthSatellite(state variables) to find distance
-    moonSat = mm.subtract(VPC.earthMoon, [[state.pn], [state.pe], [-state.pd]])
+    moonSat = mm.subtract(VPC.earthMoon, [[state.pn], [state.pe], [state.pd]])
     #calculating the norm
-    norm = math.sqrt((moonSat[0][0] ** 2) + (moonSat[1][0] ** 2) + (moonSat[2][0] ** 2))
-    return mm.scalarDivide(moonSat, norm)
+    norm = mm.scalarDivide(np.linalg.norm(moonSat), moonSat)
+    #rotation to body frame
+    rot = mm.multiply(mm.transpose(state.R), norm)
+    return rot
 
 def distanceFromSun(state):
     """Calculates the vector between the Sun and the satellite by inputting the NED positions of the satellite
     throughout orbit using Earth distances as reference points. Returns normalized vector.
     """
     # finding the difference between earthSun and earthSatellite(state variables) to find distance
-    sunSat = mm.subtract(VPC.earthSun, [[state.pn], [state.pe], [-state.pd]])
+    sunSat = mm.subtract(VPC.earthSun, [[state.pn], [state.pe], [state.pd]])
     # calculating the norm
-    norm = math.sqrt((sunSat[0][0] ** 2) + (sunSat[1][0] ** 2) + (sunSat[2][0] ** 2))
-    return mm.scalarDivide(sunSat, norm)
+    norm = mm.scalarDivide(np.linalg.norm(sunSat), sunSat)
+    # rotation to body frame
+    rot = mm.multiply(mm.transpose(state.R), norm)
+    return rot
 
 def distanceFromJupiter(state):
     """Calculates the vector between Jupiter and the satellite by inputting the NED positions of the satellite
     throughout orbit using Earth distances as reference points. Returns normalized vector.
     """
     # finding the difference between earthSun and earthSatellite(state variables) to find distance
-    jupSat = mm.add(VPC.earthJup, [[state.pn], [state.pe], [-state.pd]])
+    jupSat = mm.subtract(VPC.earthJup, [[state.pn], [state.pe], [state.pd]])
     # calculating the norm
-    norm = math.sqrt((jupSat[0][0] ** 2) + (jupSat[1][0] ** 2) + (jupSat[2][0] ** 2))
-    return mm.scalarDivide(jupSat, norm)
+    norm = mm.scalarDivide(np.linalg.norm(jupSat), jupSat)
+    # rotation to body frame
+    rot = mm.multiply(mm.transpose(state.R), norm)
+    return rot
 
 def satSurfaceArea(state):
     """Calculates the surface area of the light from the sun hitting the satellite.
     """
     #area of the satellite
     A = VPC.lengthY * VPC.lengthX
-    # Identifying the vector the satellite is pointing
-    vector =  mm.multiply([[0],[0],[-1]], mm.transpose(Rotations.euler2DCM(state.yaw, state.pitch, state.roll)))
-    unit_vector = mm.scalarDivide(vector, np.linalg.norm(vector))
-    # dot product between both vectors
-    dot_product = np.dot(distanceFromSun(state), unit_vector)
+    # rotating the satelites position in the body frame
+    rot = mm.transpose(Rotations.euler2DCM(state.yaw, state.pitch, state.roll))
+    vector =  mm.multiply(rot, [[0],[0],[-1]])
+    #solving for the unit vector
+    unit_vector = mm.scalarDivide(np.linalg.norm(vector), vector)
+    # dot product between both vectors to find angle
+    dot_product = np.dot(np.squeeze(np.asarray(distanceFromSun(state))), np.squeeze(np.asarray(unit_vector)))
     # total surface area
     surf_area = A * math.cos(dot_product)
     return surf_area
